50eefbf76f718217a1a3356039cd4049776ae9af
Krzysztof Zakrzewski <krzysztof.zakrzewski@protonmail.com>
2023-03-05 02:37:04 +0100

    crated toggle option function and option button for option popup

--- js/drawing-app-interface.js
+++ js/drawing-app-interface.js
@@ -2,16 +2,24 @@
 
 // Variables;
 
-
 const closePopupInfoBtn = document.querySelector('.close-popup-info__btn');
+const closeOptionsBtn = document.querySelector('.close-popup-options__btn');
+const openOption = document.querySelector('.options')
 
 const popupInfo = document.querySelector('.popup-info');
+const popupOptions = document.querySelector('.popup-options');
 const popupInfoActive = document.querySelector('.popup-info-active');
 
+//this function toggle info popup
 function closePopupInfo() {
-	if (popupInfo.classList.contains('popup-info-active')) {
-		popupInfo.classList.remove('popup-info-active');
-	}
+	popupInfo.classList.toggle('popup-info-active');
+}
+
+// this function toggle option popup
+function toggleOptionsPopup() {
+	popupOptions.classList.toggle('popup-options-active')
 }
 
 closePopupInfoBtn.addEventListener('click', closePopupInfo);
+closeOptionsBtn.addEventListener('click', toggleOptionsPopup)
+openOption.addEventListener('click', toggleOptionsPopup)
